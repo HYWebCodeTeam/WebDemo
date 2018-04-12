882caa2f10488efe3279188a28052efbf9fd8378
Grave robbery <GeekBBer@qq.com>
2018-04-12 14:00:40 +0800

    上传excel文件并保存到数据库返回结果视图

--- WebDemo/Controllers/HomeController.cs
+++ WebDemo/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using BLL;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -46,17 +47,76 @@ namespace WebDemo.Controllers
         /// 请求转换一个Excel文件
         /// </summary>
         /// <returns></returns>
+        [HttpPost]
         public ActionResult ConvertExcel()
         {
-            // 1、获取Excel文件对象
-            const string strFilePathName = @"E:\1.xlsx";
+            // 1、接收文件
+            var strFilePathName = GetUploadFilePathName();
+
+
+            // 2、获取Excel文件对象
             m_iExcelService.DoConvert(strFilePathName);
 
 
-            // 2、写入数据库
+            // 3、写入数据库
             m_iEntity2DBService.Save2DB(m_iExcelService);
 
 
+            return View(m_iExcelService as ExcelServerImp);
+        }
+
+
+        string GetUploadFilePathName()
+        {
+            Stream uploadStream = null;
+            FileStream fs = null;
+            string strFilePathName = string.Empty;
+
+            try
+            {
+                HttpPostedFileBase postFileBase = Request.Files["excelFile"];
+                uploadStream = postFileBase.InputStream;
+                int bufferLen = 1024;
+                byte[] buffer = new byte[bufferLen];
+                int contentLen = 0;
+
+                string fileName = Path.GetFileName(postFileBase.FileName);
+                string baseUrl = Server.MapPath("/");
+                string uploadPath = baseUrl + @"App_Data\";
+                strFilePathName = Path.Combine(uploadPath, fileName);
+
+                fs = new FileStream(strFilePathName, FileMode.Create, FileAccess.ReadWrite);
+
+                while ((contentLen = uploadStream.Read(buffer, 0, bufferLen)) != 0)
+                {
+                    fs.Write(buffer, 0, bufferLen);
+                    fs.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.StackTrace.ToString();
+            }
+            finally
+            {
+                if (null != fs)
+                {
+                    fs.Close();
+                }
+                if (null != uploadStream)
+                {
+                    uploadStream.Close();
+                }
+            }
+
+            return strFilePathName;
+        }
+
+
+
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
             return View();
         }
     }
